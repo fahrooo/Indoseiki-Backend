b87ddac95dbfd72fcac8ad365ab72b356cc4aba4
fahrooo <fahrooo2808@student.uns.ac.id>
2023-10-03 23:49:26 +0700

    Setup Router

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import cookieParser from "cookie-parser";
 import cors from "cors";
 import http from "http";
 import db from "./config/Database.js";
+import router from "./routes/index.js";
 
 dotenv.config();
 
@@ -12,15 +13,16 @@ const app = express();
 app.use(cors({ credentials: true, origin: "*" }));
 app.use(cookieParser());
 app.use(express.json());
+app.use(router);
 
 const server = http.createServer(app);
 
 try {
-    await db.authenticate();
-    console.log("Database Connected");
-  } catch (error) {
-    console.log(error);
-  }
+  await db.authenticate();
+  console.log("Database Connected");
+} catch (error) {
+  console.log(error);
+}
 
 server.listen(process.env.PORT, () =>
   console.log("Server listening on port " + process.env.PORT)
