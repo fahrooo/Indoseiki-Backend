72018d484d93f4d2176bc1b323c346b808287318
fahrooo <fahrooo2808@student.uns.ac.id>
2023-10-03 22:36:47 +0700

    Setup Connection Database

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import http from "http";
+import db from "./config/Database.js";
 
 dotenv.config();
 
@@ -13,6 +14,13 @@ app.use(cookieParser());
 app.use(express.json());
 
 const server = http.createServer(app);
+
+try {
+    await db.authenticate();
+    console.log("Database Connected");
+  } catch (error) {
+    console.log(error);
+  }
 
 server.listen(process.env.PORT, () =>
   console.log("Server listening on port " + process.env.PORT)
