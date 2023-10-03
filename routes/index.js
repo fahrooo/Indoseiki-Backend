a46c03a3c811417554b3badf4e37dbec1f01b3af
fahrooo <fahrooo2808@student.uns.ac.id>
2023-10-03 23:54:20 +0700

    Add Users Router

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
 import express from "express";
 import { Login } from "../controller/Auth.js";
+import { getUser, postUser, putUser, deleteUser } from "../controller/Users.js";
 
 const router = express.Router();
 
 // Auth
 router.post("/login", Login);
+
+// Users
+router.get("/users", getUser);
+router.post("/users/create", postUser);
+router.put("/users/update/:id", putUser);
+router.delete("/users/delete/:id", deleteUser);
 
 export default router;
