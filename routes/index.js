ad9b843065eca265e9a73bf5652966cfd3d4c94d
fahrooo <fahrooo2808@student.uns.ac.id>
2023-10-04 00:46:33 +0700

    Add Book ROuter

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import { Login } from "../controller/Auth.js";
 import { getUser, postUser, putUser, deleteUser } from "../controller/Users.js";
+import { deleteBook, getBook, postBook, putBook } from "../controller/Book.js";
 
 const router = express.Router();
 
@@ -12,5 +13,11 @@ router.get("/users", getUser);
 router.post("/users/create", postUser);
 router.put("/users/update/:id", putUser);
 router.delete("/users/delete/:id", deleteUser);
+
+// Book
+router.get("/book", getBook);
+router.post("/book/create", postBook);
+router.put("/book/update/:id", putBook);
+router.delete("/book/delete/:id", deleteBook);
 
 export default router;
