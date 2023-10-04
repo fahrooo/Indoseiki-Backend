9db7924e9e17f097acaddf07d424e7b9f5118e82
fahrooo <fahrooo2808@student.uns.ac.id>
2023-10-04 18:38:51 +0700

    Add History Router

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import { Login } from "../controller/Auth.js";
 import { getUser, postUser, putUser, deleteUser } from "../controller/Users.js";
 import { deleteBook, getBook, postBook, putBook } from "../controller/Book.js";
+import { deleteHistory, getHistory, postHistory, putHistory } from "../controller/History.js";
 
 const router = express.Router();
 
@@ -19,5 +20,11 @@ router.post("/book", getBook);
 router.post("/book/create", postBook);
 router.put("/book/update/:id", putBook);
 router.delete("/book/delete/:id", deleteBook);
+
+// History
+router.get("/history", getHistory);
+router.post("/history/create", postHistory);
+router.put("/history/update/:id", putHistory);
+router.delete("/history/delete/:id", deleteHistory);
 
 export default router;
