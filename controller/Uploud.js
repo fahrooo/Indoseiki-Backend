545acd3df4bdbe0d01a6fd4bcd803a712b121acc
fahrooo <fahrooo2808@student.uns.ac.id>
2023-10-05 20:13:04 +0700

    Create Upload Controller

---
+++ controller/Uploud.js
@@ -0,0 +1,29 @@
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+
+export const uploudImage = async (req, res) => {
+  return res.status(200).json({
+    status: 200,
+    message: "Berhasil Upload",
+  });
+};
+
+export const getImage = async (req, res) => {
+  const imagePath = path.join("images", req.params.image);
+  console.log(imagePath);
+
+  // Read the image file
+  fs.readFile(imagePath, (err, data) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send("Internal Server Error");
+    } else {
+      // Set the content type to image/jpeg (or the appropriate content type for your image)
+      res.contentType("image/jpeg");
+      // Send the image data as the response
+      res.send(data);
+    }
+  });
+};
